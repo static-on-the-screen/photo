60805f0bf44f14723a3c3b8ad321475770bb1b47
emilhilv <emilhilv@woodland.on.ca>
2025-02-03 12:13:42 -0500

    Feb. 3

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 from PIL import Image
 from urllib.request import urlopen
 import time
-def type(str):
-    x = len(str)
-    for i in range(0, x):
-        print((str)[i], end='')
-        time.sleep(0.03)
-    print('')
+import turtle
+def start():
+    turtle.clear()
+    turtle.textinput("Upload a Photo","Paste the URL of your desired photo into the box.")
+
+
+s = turtle.getscreen()
+turtle.hideturtle()
+turtle.penup()
+turtle.goto(-180, 200)
+turtle.write("Photo Editing!", font=("Calibri", 45, "normal"))
+turtle.penup()
+turtle.goto(-130, 45)
+turtle.write("Press space to start", font=("Calibri", 20, "normal"))
+turtle.onkey(start,)
 
 
 
 
 
-type("Welcome to Photo Editing!")
-time.sleep(1)
-type("To choose a photo to edit, please input the URL that leads to your picture! (Make sure it doesn't lead to a website with the picture!)")
-print("★ Input 1 for help")
-url = input()
-if url == '1':
-    print('L')
-else:
-    img = Image.open(urlopen(url))
-    img.show()
+
+
+
+
+
+
+turtle.mainloop()
